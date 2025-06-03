cc964e416b356defad366e7f27d24b04d3231b77
oteofof <wolonter228@gmail.com>
2025-06-03 20:37:41 +0300

    Update validators.py

--- utils/validators.py
+++ utils/validators.py
@@ -1,12 +1,10 @@
 def is_valid_dota_mmr(text):
-    """Проверяет, что введённый MMR для Dota 2 в допустимом диапазоне"""
     if not text.isdigit():
         return False
     mmr = int(text)
     return 50 <= mmr <= 17000
 
 def is_valid_faceit_elo(text):
-    """Проверяет, что введённый ELO для Faceit в допустимом диапазоне"""
     if not text.isdigit():
         return False
     elo = int(text)
